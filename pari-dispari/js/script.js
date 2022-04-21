d9b336e9e6351a79fd44561621831d7a4a732053
Zenrool <lorenzopapperini@hotmail.it>
2022-04-21 16:59:54 +0200

    Funzione per giocare a pari e dispari e controllo vincitore in base al radiobutton indicato

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,5 +1,23 @@
+// costanti dom 
+let res = document.querySelector(".form > p");
 // Funzioni 
-function odd(num) {
-    return num%2;
+function isOdd(num) {
+    return Boolean(num%2);
+    // Dava problemi === se ritornava un numero
+}
+function btnPlay() {
+    const playerNumber = parseInt(document.getElementById("number").value);
+    const radiOdd = document.getElementById("odd").checked;
+    const cpuNumber = Math.floor (Math.random() * 5 + 1);
+
+    if (isNaN(playerNumber) || playerNumber < 1 || playerNumber > 5) {
+        res.innerHTML = "Dammi un numero da uno a cinque ";
+        return 0;
+    }
+    const sum = playerNumber + cpuNumber;
+    const odd = isOdd(sum);
+    res.innerHTML = `La somma di ${playerNumber} e ${cpuNumber} (numero della cpu) è ${sum} ed è ${odd ? `dispari` : `pari`} ed hai ${radiOdd === odd ? `vinto :-)` : `perso :-(`} `;
+    // console.log(odd , " odd");
+    // console.log(radiOdd , " Radio odd");
 
 }
